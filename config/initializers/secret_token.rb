9a52438aaab659f6a9fcf73caae561979bb2d19e
Carsten Senkfeil <noheadlights@me.com>
2012-09-22 00:57:02 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pickr::Application.config.secret_token = '300123822d618df179d05df43d1241affb4fda2fc521a45c2127b2f05d264e7eb8cbc2d8039f40f72357a4dda71b547de21798d7165432876caa28389308ba08'
